1d8d7b75eb59f0f23abdabf2988d56eea4cae520
Valeriia Isaienko <lerika@MacBook-Pro.local>
2023-01-24 14:09:09 -0800

    first commit

---
+++ src/ShoppingList.js
@@ -0,0 +1,62 @@
+import { Component } from "react";
+import image from './bag.png';
+
+export class ShoppingList extends Component {
+        state = {
+                        userInput: " ",
+                        shoppingList: [ ]
+    }
+    onChangeEvent( e ){
+        this.setState ({ userInput: e });
+    }
+    addItem( input ){
+        if (input === " "){
+            alert("Please enter an item");
+        } else{
+        let listArray = this.state.shoppingList;
+        listArray.push( input )
+        this.setState({ shoppingList: listArray, userInput:" " })
+    }
+    }
+    deleteItem(){
+        let listArray = this.state.shoppingList;
+        listArray = [ ];
+        this.setState({ shoppingList : listArray })
+    }
+    crossedWord(e){
+        const li = e.target;
+        li.classList.toggle("crossed");
+    }
+    onFormSubmit(e){
+        e.preventDefault();
+    }
+    render(){
+        return(
+            <div>
+            <form onSubmit = { this.onFormSubmit }>
+                <div className="container">
+                <input type="text"
+                        placeholder="What do you want to buy?" 
+                        onChange = {(event) => this.onChangeEvent(event.target.value)} 
+                        value= { this.state.userInput } />
+                </div>
+                <div className="container">
+                    <button onClick = { () => this.addItem( this.state.userInput )} className="btn add">Add</button>
+                </div>
+                <div>
+                    <ul>
+                        { this.state.shoppingList.map((item, index) => (
+                            <li onClick = { this.crossedWord } key= { index }>
+                                <img src= { image } alt="bag" width="40px" />{ item }</li>
+                        ))}
+                    </ul>
+                </div>
+                
+                <div className="container">
+                    <button  onClick = { () => this.deleteItem( this.state.userInput )} className="btn delete">Delete</button>
+                </div>
+            </form>
+            </div>
+        )
+    }
+}
